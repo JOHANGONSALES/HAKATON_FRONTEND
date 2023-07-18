5a1c1e011b5da728be699e8c13f32245235fede5
JOHAN <jagg.gonsales@gmail.com>
2023-07-18 23:09:27 +0200

    inicializacion

---
+++ hooks_react/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      
+      <button type="button" class="btn btn-secondary">Secondary</button>
+      
+    </div>
+  );
+}
+
+export default App;
