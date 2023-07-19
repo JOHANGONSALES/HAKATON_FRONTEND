129a96d885f7db981cec738ba0f0d3c1258c8b53
Cindy Leiva <cindyleiva376@gmail.com>
2023-07-20 00:09:56 +0200

    creacion de carpeta assets / personalizacion de estilos generales en index.css incluyendo root de colores y link de tipografia / creacion de componente card no finalizado

--- hooks_react/src/App.js
+++ hooks_react/src/App.js
@@ -1,9 +1,10 @@
-
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div>
+      <Card/>
       
       <button type="button" class="btn btn-secondary">Secondary</button>
       
