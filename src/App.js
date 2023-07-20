99c88b419eff2f955e8ba2b06c666bd55662e4c3
JOHAN <jagg.gonsales@gmail.com>
2023-07-20 12:15:50 +0200

    cambio footer

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Footer from './components/Footer';
+
+import Card from './components/Card';
+
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div>
+    
+      <Header/>
+      <Card/>
+      
+      
+
+
+      
+        
+
+    
+      <Footer/>
+</div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logosvg from './asset/logosvg1.svg'
+import copy from './asset/copy.svg'
+import './Footer.css';
+
+const Footer = () => {
+  return (
+   <footer>
+    <div className="footer-content">
+        <img className='logo' src={logosvg} alt="Logo" />
+        <img className='copy' src={copy} alt="copyray" />
+    </div>
+   </footer>
+
+  )
+};
+
+export default Footer;
