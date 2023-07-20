360bb9539b1b15c853dddc4cd75d02e5011ef680
JOHAN <jagg.gonsales@gmail.com>
2023-07-20 09:28:07 +0200

    footer

--- hooks_react/src/App.js
+++ hooks_react/src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       
       <button type="button" class="btn btn-secondary">Secondary</button>
-      
+      <Footer/>
     </div>
   );
 }
